d48602e5d8ba97df870710bbe235109c60441f41
Johnny Martin <johnny@johnny5th.com>
2014-05-18 21:22:44 -0500

    Completed label hiding to close #13

--- sites/all/themes/main/src/js/script.js
+++ sites/all/themes/main/src/js/script.js
@@ -17,7 +17,26 @@
     $('.menu-button').click(function() {
       $('.primary-navigation').toggleClass('open');
       return false;
-    })
+    });
+
+    // Label hiding
+    var form_elements = $('.form-type-textfield, .webform-component-textfield, .webform-component-textfield, .webform-component-textarea, .form-email, .form-type-password, .webform-component-email');
+
+    form_elements.find('input, textarea').change(function() {
+      var elem = $(this);
+      if(elem.val() != "")
+        $('label[for="'+elem.attr('id')+'"]').hide();
+    });
+
+    form_elements.find('input, textarea').focus(function() {
+      var elem = $(this);
+      $('label[for="'+elem.attr('id')+'"]').hide();
+    }).blur(function() {
+      var elem = $(this);
+      if(elem.val() == "")
+        $('label[for="'+elem.attr('id')+'"]').show();
+    });
+    // End Label Hiding
 
 	}); // End Ready
 
